6caa3bf1180c6e83f65443ceac15bd832b0b1458
pierrerrrr <pierrestorniolo@gmail.com>
2025-09-12 17:21:55 -0300

    core: implement query parameter extraction and enhance route handling

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,5 +1,8 @@
 export function index({ req, res, database }) {
     const tickets = database.select("tickets");
 
+    const { status } = req.query;
+    console.log(status);
+
     return res.end(JSON.stringify(tickets))
 }
