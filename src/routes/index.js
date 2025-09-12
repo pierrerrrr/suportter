0d3af75c22bf3d2d730269aa4a532e978e9a0cbb
pierrerrrr <pierrestorniolo@gmail.com>
2025-09-11 21:17:40 -0300

    core: implement route parsing functionality and fix route matching logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 // arquivo que centraliza as rotas
 import { tickets } from './tickets.js';
+import { parseRoutPath } from '../utils/parseRoutePath.js';
 
-export const routes = [...tickets];
+export const routes = [...tickets].map((route) => ({
+    ...route,
+    path: parseRoutPath(route.path)
+}))
